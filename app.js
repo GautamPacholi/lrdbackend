719f2f96387d06fab5612d520c46a0c1506d5954
gautam <gautampachauli22@gmail.com>
2024-07-08 10:12:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express();
+app.get('/api',(req,res)=>{
+    res.send('<h1> hello world');
+});
+
+const port=process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log("listening to port 3000");
+});
